bea63bdaa047e67e86c2c9aabe9d7adca65770ec
darmaia <darmaia@gmail.com>
2023-12-04 15:32:45 -0300

    main.tf 3

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 resource "aws_instance" "example_server" {
     ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
-    subnet_id = var.subnet_id
+    subnet_id = "subnet-91935fce"
     associate_public_ip_address = true
+    ebs_block_device {
+      device_name = "/dev/sda1"
+      volume_size = 20
+    }
   
     tags = {
       Name = "Teste EC2"
