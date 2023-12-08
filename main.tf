c1123da5fe40dff859477eee036825cbc36d405a
darmaia <darmaia@gmail.com>
2023-12-08 16:41:47 -0300

    main.tf 9

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "example_server" {
     ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
-    
     subnet_id = "subnet-91935fce"
     associate_public_ip_address = true
     ebs_block_device {
