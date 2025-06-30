afcb40f901186d81731b623ae56630f8ff8a4b7e
diogo.maia <diogo.maia@sistemastecnol.com.br>
2025-06-30 11:47:33 -0300

    Teste

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "example_server" {
+resource "aws_instance" "example_server1" {
     ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
     subnet_id = "subnet-91935fce"
