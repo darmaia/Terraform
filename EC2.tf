95de727f24d74aa749cd0c61d06cf18c4c59f9a5
git <darmaia@gmail.com>
2025-06-30 16:23:00 -0300

    Criei o EC2

---
+++ EC2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "Teste" {
+    ami = "ami-0fc5d935ebf8bc3bc"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-91935fce"
+    associate_public_ip_address = true
+    ebs_block_device {
+      device_name = "/dev/sda1"
+      volume_size = 20
+    }
+}
--- main.tf
+++ main.tf
@@ -2,17 +2,6 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "Teste" {
-    ami = "ami-0fc5d935ebf8bc3bc"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-91935fce"
-    associate_public_ip_address = true
-    ebs_block_device {
-      device_name = "/dev/sda1"
-      volume_size = 20
-    }
-}
-
 resource "aws_db_instance" "default" {
     instance_class = "db.t3.micro"
     allocated_storage = 5
