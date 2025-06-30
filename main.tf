a603c9e81610c40f49462a77e5bda8485d4c5e18
git <darmaia@gmail.com>
2025-06-30 14:29:58 -0300

    Teste1

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "example_server1" {
+resource "aws_instance" "Teste" {
     ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
     subnet_id = "subnet-91935fce"
