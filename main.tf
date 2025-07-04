38415d7be9174692cafc5a425d83c2099e6716a9
darmaia <138609721+darmaia@users.noreply.github.com>
2025-07-04 13:58:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-    region = "us-east-1"
-}
+#provider "aws" {
+#    region = "us-east-1"
+#}
 
-resource "aws_db_instance" "default" {
-    instance_class = "db.t3.micro"
-    allocated_storage = 5
-    engine = "mariadb"
-    engine_version = "10.6.14"
-    username = "${var.username}"
-    password = "${var.senhabd}"
-    publicly_accessible = true
-}
+#resource "aws_db_instance" "default" {
+#    instance_class = "db.t3.micro"
+#    allocated_storage = 5
+#    engine = "mariadb"
+#    engine_version = "10.6.14"
+#    username = "${var.username}"
+#    password = "${var.senhabd}"
+#    publicly_accessible = true
+#}
