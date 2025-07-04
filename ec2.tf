22dd00b2e73c15297b40eb2b8ce1498102dbb767
darmaia <138609721+darmaia@users.noreply.github.com>
2025-07-04 13:57:33 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,16 @@
-resource "aws_instance" "Teste" {
+resource "aws_instance" "SRV1" {
     ami = "ami-0fc5d935ebf8bc3bc"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-91935fce"
+    instance_type = "t2.small"
+    subnet_id = "subnet-08cae06e825cf78b8"
     associate_public_ip_address = true
     ebs_block_device {
       device_name = "/dev/sda1"
-      volume_size = 20
+      volume_size = 30
     }
+    tags = {
+        Name = "SRV1"
+    }
+   #lifecycle {
+    #ignore_changes = all
+  #}
 }
